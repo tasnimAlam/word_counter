4eb3fa1763537c738181031df28803e6c86c1cd2
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2021-08-20 17:15:26 +0600

    Count words from input file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
@@ -24,7 +25,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
+    let counts = get_word_count(&contents);
+
+    println!("count is {:?}", counts);
+    Ok(())
+}
+
+pub fn get_word_count(contents: &str) -> HashMap<&str, u32> {
+    let mut map = HashMap::new();
     println!("with text: \n {}", contents);
 
-    Ok(())
+    for word in contents.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    map
 }
